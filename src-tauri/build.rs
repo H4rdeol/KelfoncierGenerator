41969c2ce8dd069590b41b355b5908472d074908
Hardeol <leonard.oursel@epitech.eu>
2025-04-30 16:40:10 +0200

    Refactor CI workflow to export secrets to GitHub environment variables and update Cargo.toml build configuration

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,16 @@
 fn main() {
+    println!("cargo:rerun-if-env-changed=API_KEY");
+    println!("cargo:rerun-if-env-changed=EMAIL");
+    println!("cargo:rerun-if-env-changed=PASSWORD");
+
+    if let Ok(key) = std::env::var("API_KEY") {
+        println!("cargo:rustc-env=API_KEY={}", key);
+    }
+    if let Ok(email) = std::env::var("EMAIL") {
+        println!("cargo:rustc-env=EMAIL={}", email);
+    }
+    if let Ok(pw) = std::env::var("PASSWORD") {
+        println!("cargo:rustc-env=PASSWORD={}", pw);
+    }
     tauri_build::build()
 }
